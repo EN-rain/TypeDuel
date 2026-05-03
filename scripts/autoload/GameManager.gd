473166698c8ec54386d4bf3a023e3331603bb77e
EN-rain <nieves.edriancit2011@gmail.com>
2026-05-03 17:16:55 +0800

    Initialize project structure and add GameManager

---
+++ scripts/autoload/GameManager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+## Global Game Manager Singleton
+## Register this in Project Settings > Autoload as 'Game' or 'GameManager'
+
+signal game_started
+signal game_ended(results: Dictionary)
+
+var current_score: int = 0
+var is_game_active: bool = false
+
+func start_game() -> void:
+	is_game_active = true
+	current_score = 0
+	game_started.emit()
+	print("Game Started")
+
+func end_game(results: Dictionary = {}) -> void:
+	is_game_active = false
+	game_ended.emit(results)
+	print("Game Ended with results: ", results)
